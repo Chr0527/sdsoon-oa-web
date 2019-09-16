1c62542b106e22f101d8cb580ec85848da2182e8
Chr0527 <1015670600@qq.com>
2019-09-16 14:50:21 +0800

    init

--- src/main/java/com/chryl/SdsoonOaWebApplication.java
+++ src/main/java/com/chryl/SdsoonOaWebApplication.java
@@ -2,12 +2,19 @@ package com.chryl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SdsoonOaWebApplication {
+public class SdsoonOaWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SdsoonOaWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SdsoonOaWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SdsoonOaWebApplication.class);
+    }
 
 }
